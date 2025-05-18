53ca3ff09e6c84723884e380abfba96a375e5aa5
Nuriddin Mahmud <154328556+nuriddinmahmud@users.noreply.github.com>
2025-05-18 18:18:03 +0500

    added cache to app and maints

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,23 @@
 import { Module } from '@nestjs/common';
+import { CacheModule } from '@nestjs/cache-manager'; 
 import { AuthorModule } from './author/author.module';
 import { BookModule } from './book/book.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { AppController } from './app.controller'; 
+import { AppService } from './app.service'; 
 
 @Module({
   imports: [
+    CacheModule.register({
+      isGlobal: true, 
+      ttl: 60,        
+      max: 100,       
+    }),
     PrismaModule,
     AuthorModule,
     BookModule,
   ],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import { AuthorModule } from './author/author.module';
-import { BookModule } from './book/book.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const config = new DocumentBuilder()
-    .setTitle('Book API')
-    .setDescription('Author & Book CRUD REST API with Swagger')
-    .setVersion('1.0')
+    .setTitle('📚 Book API')
+    .setDescription('NestJS CRUD API for Authors and Books')
+    .setVersion('1.0.0')
+    .addTag('Author') 
+    .addTag('Book') 
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('docs', app, document);
+  SwaggerModule.setup('docs', app, document); 
 
   await app.listen(3000);
+  console.log(`Application is running on: http://localhost:3000`);
+  console.log(`Swagger docs: http://localhost:3000/docs`);
 }
 bootstrap();
